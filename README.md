4786728f9d83388e7c1fc0ea740aab044592527a
hashwarlock <hashwarlock@protonmail.com>
2022-11-26 21:26:18 -0600

    Update phala init to create a template contract and download phala local testnet binaries

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ Clone this repo, run `yarn build`, and link it to swanky-cli:
 swanky plugins:install [PATH_TO_REPO]
 ```
 
-Now you can run `swanky phala node download`
+Now you can run `swanky phala init`
 
 ## swanky-core
 
@@ -37,35 +37,38 @@ https://github.com/oclif/plugin-plugins
 # Generated README:
 
 <!-- toc -->
-
-- [Usage](#usage)
-- [Commands](#commands)
+* [swanky-plugin-phala](#swanky-plugin-phala)
+* [Generated README:](#generated-readme)
+* [Usage](#usage)
+* [Commands](#commands)
 <!-- tocstop -->
 
 # Usage
 
 <!-- usage -->
-
 ```sh-session
 $ npm install -g @astar-network/swanky-plugin-phala
 $ phala COMMAND
 running command...
 $ phala (--version)
-@astar-network/swanky-plugin-phala/0.2.0 darwin-x64 node-v18.2.0
+@astar-network/swanky-plugin-phala/0.2.0 linux-x64 node-v18.12.1
 $ phala --help [COMMAND]
 USAGE
   $ phala COMMAND
 ...
 ```
-
 <!-- usagestop -->
 
 # Commands
 
 <!-- commands -->
-
-- [`phala help [COMMAND]`](#phala-help-command)
-- [`phala phala node download`](#phala-phala-node-download)
+* [`phala help [COMMAND]`](#phala-help-command)
+* [`phala phala compile [CONTRACTNAME]`](#phala-phala-compile-contractname)
+* [`phala phala INIT [PROJECTNAME]`](#phala-phala-compile-projectname)
+* [`phala phala stack`](#phala-phala-stack)
+* [`phala phala stack-setup [FILE]`](#phala-phala-stack-setup-file)
+* [`phala phala test [FILE]`](#phala-phala-test-file)
+* [`phala phala typings [FILE]`](#phala-phala-typings-file)
 
 ## `phala help [COMMAND]`
 
@@ -87,11 +90,111 @@ DESCRIPTION
 
 _See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v5.1.19/src/commands/help.ts)_
 
-## `phala phala node download`
+## `phala phala compile [CONTRACTNAME]`
+
+TODO
 
 ```
 USAGE
-  $ phala phala node download
+  $ phala phala compile [CONTRACTNAME] [-w <value>] [-r]
+
+FLAGS
+  -r, --force         compile in release mode
+  -w, --name=<value>  watch for changes
+
+DESCRIPTION
+  TODO
+
+EXAMPLES
+  $ phala phala compile
+```
+## `phala phala init [PROJECTDIR]`
+
+Generate a new Phat Contract environment and download local testnet binares.
+
+```
+USAGE
+  $ phala phala init [PROJECTNAME]
+
+FLAGS
+  -v, --verbose
+
+DESCRIPTION
+  describe the command here
+
+EXAMPLES
+  $ phala phala init my_new_project
 ```
 
+## `phala phala stack`
+
+Starting local stack (node + pruntime + pherry)
+
+```
+USAGE
+  $ phala phala stack
+
+DESCRIPTION
+  Starting local stack (node + pruntime + pherry)
+
+EXAMPLES
+  $ phala phala stack
+```
+
+## `phala phala stack-setup [FILE]`
+
+TODO
+
+```
+USAGE
+  $ phala phala stack-setup [FILE] [-n <value>] [-f]
+
+FLAGS
+  -f, --force
+  -n, --name=<value>  name to print
+
+DESCRIPTION
+  TODO
+
+EXAMPLES
+  $ phala phala stack-setup
+```
+
+## `phala phala test [FILE]`
+
+TODO
+
+```
+USAGE
+  $ phala phala test [FILE] [-n <value>] [-f]
+
+FLAGS
+  -f, --force
+  -n, --name=<value>  name to print
+
+DESCRIPTION
+  TODO
+
+EXAMPLES
+  $ phala phala test
+```
+
+## `phala phala typings [FILE]`
+
+TODO
+
+```
+USAGE
+  $ phala phala typings [FILE] [-n <value>] [-f]
+
+FLAGS
+  -f, --force
+  -n, --name=<value>  name to print
+
+DESCRIPTION
+  TODO
+
+EXAMPLES
+  $ phala phala typings
+```
 <!-- commandsstop -->
--- src/commands/phala/init.ts
+++ src/commands/phala/init.ts
@@ -1,23 +1,128 @@
-import {Command} from '@oclif/core'
+import {Command, Flags} from '@oclif/core'
+import {
+  copyTemplateFiles,
+  processTemplates,
+  Spinner,
+} from "@astar-network/swanky-core";
+import { paramCase, pascalCase, snakeCase } from "change-case";
+import inquirer from 'inquirer';
+
+import { choice, email, name, pickLanguage, pickTemplate } from "../../lib/prompts";
 import execa from 'execa'
 import path = require("node:path")
+import { readdirSync } from "node:fs";
+
+export function getTemplates(language = "pink") {
+  const templatesPath = path.resolve(__dirname, "../..", "templates");
+  const contractTemplatesPath = path.resolve(templatesPath, "contracts", language);
+  const fileList = readdirSync(contractTemplatesPath, {
+    withFileTypes: true,
+  });
+  const contractTemplatesList = fileList
+    .filter((entry) => entry.isDirectory())
+    .map((entry) => ({
+      message: entry.name,
+      value: entry.name,
+    }));
+
+  return { templatesPath, contractTemplatesPath, contractTemplatesList };
+}
 
 export default class PhalaInit extends Command {
-  static description = 'Init project (creates required files and directories)'
+  static description = 'Generate a new Phat Contract environment'
 
   static examples = [
     '<%= config.bin %> <%= command.id %>',
   ]
 
+  static flags = {
+    "phala-node": Flags.boolean(),
+    template: Flags.string({
+      options: getTemplates().contractTemplatesList.map((template) => template.value),
+    }),
+    verbose: Flags.boolean({ char: "v" }),
+  };
+
+  static args = [
+    {
+      name: "projectName",
+      required: true,
+      description: "directory name of new project",
+    },
+  ];
+
   public async run(): Promise<void> {
+    const { args, flags } = await this.parse(PhalaInit);
+
+    const projectPath = path.resolve(args.projectName);
+
+    const { contractLanguage } = await inquirer.prompt([pickLanguage()]);
+
+    const templates = getTemplates();
+
+    const questions = [
+      pickTemplate(templates.contractTemplatesList),
+      name("contract", (ans) => ans.contractTemplate, "What should we name your contract?"),
+      name(
+        "author",
+        () => execa.commandSync("git config --get user.name").stdout,
+        "What is your name?"
+      ),
+      email(),
+    ];
+
+    if (!flags["phala-node"]) {
+      questions.push(choice("usePhalaNode", "Do you want to download Phala node components for local testnet?"));
+    }
+
+    const answers = await inquirer.prompt(questions);
+
+    const spinner = new Spinner(flags.verbose);
     this.log(`Initializing`)
 
-    const projectPath = path.resolve()
-    const devphasePath = path.resolve(projectPath, "devphase")
+    await spinner.runCommand(
+      () =>
+        copyTemplateFiles(
+          templates.templatesPath,
+          path.resolve(templates.contractTemplatesPath, answers.contractTemplate),
+          answers.contractName,
+          projectPath
+        ),
+      "Copying template files"
+    );
 
-    await execa.command(`yarn install`, { cwd: devphasePath })
-    await execa.command(`yarn compile`, { cwd: devphasePath })
+    await spinner.runCommand(
+      () =>
+        processTemplates(projectPath, {
+          project_name: paramCase(args.projectName),
+          author_name: answers.authorName,
+          author_email: answers.email,
+          swanky_version: this.config.pjson.version,
+          contract_name: answers.contractName,
+          contract_name_snake: snakeCase(answers.contractName),
+          contract_name_pascal: pascalCase(answers.contractName),
+          contract_language: contractLanguage,
+        }),
+      "Processing templates"
+    );
 
-    await execa.command(`yarn devphase init`, { cwd: projectPath })
+    await spinner.runCommand(
+      () => execa.command("git init", { cwd: projectPath }),
+      "Initializing git"
+    );
+
+    if (flags["phala-node"] || answers.usePhalaNode) {
+      const basePath = path.resolve();
+      await spinner.runCommand(
+        () => execa.command(`yarn install`, { cwd: projectPath }),
+        "Setting up repo..."
+      );
+      await spinner.runCommand(
+        () => execa.command(`yarn devphase init`, { cwd: projectPath }),
+        "Downloading Phala binaries for local testnet...");
+    }
+
+    this.log("Phat Contract project successfully initialised!");
+
   }
 }
