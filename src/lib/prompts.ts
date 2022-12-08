0bd61628c21a66f83d0c55e95e0d3f45143a422e
Joshua W <64296537+HashWarlock@users.noreply.github.com>
2022-12-08 02:53:30 -0600

    Initial Phat Contract Support (#16)
    * Add devphase as submodule, add phat hello and http client contract templates.
    
    * Remove contract command for now
    
    * Add test command to for installing and compiling devphase
    
    * Add phala init and phala stack commands that map to devphase init and devphase stack
    
    * Update phala init to create a template contract and download phala local testnet binaries
    
    * Remove devphase config file to prevent error during init

---
+++ src/lib/prompts.ts
@@ -0,0 +1,54 @@
+import { Answers, ListQuestion, Question } from "inquirer"
+
+export function pickTemplate(
+  templateList: { message: string; value: string }[]
+): ListQuestion<Answers> {
+  if (!templateList || !templateList.length) throw new Error("Template list is empty!");
+  return {
+    name: "contractTemplate",
+    type: "list",
+    choices: templateList,
+    message: "Which contract template should we use?",
+  };
+}
+
+export function name(
+  subject: string,
+  initial?: (answers: Answers) => string,
+  questionText?: string
+): Question<Answers> {
+  const question: Question = {
+    name: `${subject}Name`,
+    type: "input",
+    message: questionText || `What name should we use for ${subject}?`,
+  };
+  if (initial) question.default = initial;
+  return question;
+}
+
+export function email(initial?: string, questionText?: string): Question<Answers> {
+  const question: Question = {
+    name: "email",
+    type: "input",
+    message: questionText || "What is your email?",
+  };
+  if (initial) question.default = initial;
+  return question;
+}
+
+export function choice(subject: string, questionText: string): Question<Answers> {
+  return {
+    name: subject,
+    type: "confirm",
+    message: questionText,
+  };
+}
+
+export function pickLanguage(): ListQuestion<Answers> {
+  return {
+    name: "contractLanguage",
+    type: "list",
+    choices: ["pink"],
+    message: "Which contract template should we use?",
+  };
+}
